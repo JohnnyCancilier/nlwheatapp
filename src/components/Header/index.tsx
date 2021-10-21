3d037889ef1005d9c751dcc1580e915e901887c3
Johnny Cancilier <jhony.cancilier@hotmail.com>
2021-10-21 00:38:30 -0300

    ATT7

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
-import LogoSvg from '../../assets/logo.svg'
+import LogoSvg from '../../assets/logo.svg';
+import { UserPhoto } from '../UserPhoto';
+
 
 export function Header() {
     return (
         <View style={styles.container}>
             <LogoSvg />
+
+            <UserPhoto imageUri='https://github.com/johnnycancilier.png' />
+
             <TouchableOpacity>
                 <Text style={styles.lougoutText}>
                     Sair
