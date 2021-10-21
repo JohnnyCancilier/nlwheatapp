3b246c6e698f95206269e3349c67077234cf0e0d
Johnny Cancilier <jhony.cancilier@hotmail.com>
2021-10-20 21:04:18 -0300

    att3

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { View, Text } from 'react-native'
+import { styles } from './style'
 
 export function Home() {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Olá React Native</Text>
         </View>
     )
